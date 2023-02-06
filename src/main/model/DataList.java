1eea1798225d1d66d18b6affd8f3be3fe14a8fdf
Frederick Wang <79602963+DangerGuy@users.noreply.github.com>
2023-02-05 17:21:26 -0800

    Added all methods for DataList.java

--- src/main/model/DataList.java
+++ src/main/model/DataList.java
@@ -1,6 +1,8 @@
 package model;
 
-import java.util.ArrayList;
+import java.util.*;
+
+import static java.lang.Math.*;
 
 
 // Represents a data list having a name, list length, and associated statistics
@@ -55,4 +57,69 @@ public class DataList {
     public void addNum(double number) {
         numList.add(number);
     }
+
+    public double calcOneSampleZStat(double nullMean, double popSD) {
+        return (listMean - nullMean) / (popSD / sqrt(listLength));
+    }
+
+    public double calcOneSampleZStatProp(double nullProp) {
+        return (listMean - nullProp) / (sqrt(nullProp * (1 - nullProp) / listLength));
+    }
+
+    public double calcOneSampleTStat(double nullMean) {
+        return (listMean - nullMean) / (listSD / sqrt(listLength));
+    }
+
+    public String calcConfIntervalProp(double confLevel) {
+        double z;
+        double lowerBound;
+        double upperBound;
+
+        z = getZ(confLevel);
+
+        lowerBound = listMean - (z * sqrt(listMean * (1 - listMean) / listLength));
+        upperBound = listMean + (z * sqrt(listMean * (1 - listMean) / listLength));
+
+        return "[" + lowerBound + ", " + upperBound + "]";
+    }
+
+    public String calcConfInterval(double confLevel) {
+        double z;
+        double lowerBound;
+        double upperBound;
+
+        z = getZ(confLevel);
+
+        lowerBound = listMean - (z * listSD / sqrt(listLength));
+        upperBound = listMean + (z * listSD / sqrt(listLength));
+
+        return "[" + lowerBound + ", " + upperBound + "]";
+    }
+
+    private static double getZ(double confLevel) {
+        double z;
+        if (confLevel == 0.997) {
+            z = 3;
+        } else if (confLevel == 0.99) {
+            z = 2.576;
+        } else if (confLevel == 0.95) {
+            z = 1.96;
+        } else if (confLevel == 0.9) {
+            z = 1.645;
+        } else if (confLevel == 0.8) {
+            z = 1.282;
+        } else if (confLevel == 0.75) {
+            z = 1.15;
+        } else if (confLevel == 0.68) {
+            z = 1.15;
+        } else {
+            z = 0;
+        }
+        return z;
+    }
+
+    public void sortList() {
+        Collections.sort(numList);
+    }
+
 }
